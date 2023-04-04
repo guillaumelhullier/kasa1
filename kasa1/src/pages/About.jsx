2bff4086c1da97d8ccdd66dfdfbd34d29564963c
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2023-04-04 17:07:42 +0200

    About Style

--- kasa1/src/pages/About.jsx
+++ kasa1/src/pages/About.jsx
@@ -1,14 +1,54 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import banniere from "../assets/banniere-about.jpg";
 import Collapse from "../components/Collapse";
+
+const questions = [
+  {
+    id: 1,
+    title: "Fiabilité",
+    description:
+      "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+  },
+  {
+    id: 2,
+    title: "Respect",
+    description:
+      "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+  },
+  {
+    id: 3,
+    title: "Service",
+    description:
+      "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+  },
+  {
+    id: 4,
+    title: "Sécurité",
+    description:
+      "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+  },
+];
 
 const About = () => {
   return (
     <div>
-      <Header />
-      <Footer />
-      <Collapse />
+      <Header showBorder2={true} />
+
+      <div className="banniere-about-container">
+        <img
+          className="banniere-about"
+          src={banniere}
+          alt="banniere A Propos"
+        />
+      </div>
+      <div className="collapse-about-margin">
+        {questions.map((question) => (
+          <Collapse key={question.id} question={question} />
+        ))}
+      </div>
+      <Footer marginTop={282} />
     </div>
   );
 };
